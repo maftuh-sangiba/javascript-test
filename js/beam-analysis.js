11ad48b7ba1731331fb217cb07b49de690731b1c
Maftuh sangiba <mukhamadmaftuhsangiba@gmail.com>
2024-03-09 19:24:04 +0700

    Finish twoSpanUnequal

--- js/beam-analysis.js
+++ js/beam-analysis.js
@@ -186,26 +186,157 @@ BeamAnalysis.analyzer.twoSpanUnequal = function (beam, load) {
 
 BeamAnalysis.analyzer.twoSpanUnequal.prototype = {
     getDeflectionEquation: function (beam, load) {
-        return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+        const { primarySpan, secondarySpan } = beam;
+        const { EI, j2 } = beam.material.properties;
+
+        const totalLength = primarySpan + secondarySpan;
+        const M1 = -((load * Math.pow(secondarySpan, 3)) + (load * Math.pow(primarySpan, 3))) / (8 * (primarySpan + secondarySpan));
+        const R1 = (M1 / primarySpan) + ((load * primarySpan) / 2);
+        const R3 = (M1 / secondarySpan) + ((load * secondarySpan) / 2);
+        const R2 = (load * primarySpan) + (load * secondarySpan) - R1 - R3;
+        const results = []
+        const resultsV = []
+
+        let i = 0
+        let isCounting = true
+
+        while (isCounting) {
+            let x = 0
+            const previousX = results[i - 1] || 0
+            const previousXtwice = results[i - 2]
+
+            if (i == 0) {
+                x = 0
+            } else if (previousX === 0) {
+                x = previousX + totalLength / 10;
+            } else if (Math.abs(previousX - primarySpan) <= totalLength / 10 && previousX - primarySpan < 0) {
+                x = primarySpan;
+            } else if (previousX < primarySpan && previousX !== R1 / load && previousXtwice !== R1 / load && Math.abs(previousX - R1 / load) <= totalLength / 10) {
+                x = R1 / load;
+            } else if (Math.abs(previousX - totalLength) < totalLength / 10) {
+                x = totalLength;
+            } else if (previousX > primarySpan && previousX !== totalLength - (R3 / load) && previousXtwice !== totalLength - (R3 / load) && Math.abs(previousX - (totalLength - (R3 / load))) < totalLength / 10) {
+                x = totalLength - (R3 / load);
+            } else if (previousX === R1 / load || previousX === totalLength - (R3 / load)) {
+                x = previousXtwice + totalLength / 10;
+            } else {
+                x = previousX + totalLength / 10;
+            }
+
+            results.push(x);
+
+            const currentKey = x
+            const previousKey = results[i - 1] || 0
+
+            if (i > 0) {
+                resultsV[i - 1] = calculateXLessThan12({ currentKey: previousKey })
+                if (i >= 13) {
+                    resultsV[i - 1] = calculateXGreaterThan13({ currentKey: previousKey })
+                }
+            }
+
+            isCounting = x < totalLength
+
+            if (!isCounting) {
+                resultsV[i] = calculateXGreaterThan13({ currentKey })
+            }
+
+            i++
         }
+
+        function calculateXLessThan12({ currentKey }) {
+            return ((currentKey / (24 * (EI / (1000 * 1000 * 1000))) * ((4 * R1 * (Math.pow(currentKey, 2))) - (load * (Math.pow(currentKey, 3))) + (load * (Math.pow(primarySpan, 3))) - (4 * R1 * (Math.pow(primarySpan, 2))))) * 1000 * j2);
+        }
+
+
+        function calculateXGreaterThan13({ currentKey }) {
+            return ((((R1 * currentKey / 6) * (Math.pow(currentKey, 2) - Math.pow(primarySpan, 2))) + ((R2 * currentKey / 6) * ((Math.pow(currentKey, 2)) - (3 * primarySpan * currentKey) + (3 * Math.pow(primarySpan, 2)))) - (R2 * (Math.pow(primarySpan, 3)) / 6) - ((load * currentKey / 24) * ((Math.pow(currentKey, 3)) - (Math.pow(primarySpan, 3))))) / (EI / (1000 * 1000 * 1000)) * 1000 * j2);
+        }
+
+        return {
+            x: results,
+            y: resultsV
+        };
     },
 
     getBendingMomentEquation: function (beam, load) {
-        return function (x) {
-            return {
-                x: x,
-                y: null
-            };
+        const { primarySpan, secondarySpan } = beam;
+
+        const totalLength = primarySpan + secondarySpan;
+        const M1 = -((load * Math.pow(secondarySpan, 3)) + (load * Math.pow(primarySpan, 3))) / (8 * (primarySpan + secondarySpan));
+        const R1 = (M1 / primarySpan) + ((load * primarySpan) / 2);
+        const R3 = (M1 / secondarySpan) + ((load * secondarySpan) / 2);
+        const R2 = (load * primarySpan) + (load * secondarySpan) - R1 - R3;
+        const results = []
+        const resultsV = []
+
+        let i = 0
+        let isCounting = true
+
+        while (isCounting) {
+            let x = 0
+            const previousX = results[i - 1] || 0
+            const previousXtwice = results[i - 2]
+
+            if (i == 0) {
+                x = 0
+            } else if (previousX === 0) {
+                x = previousX + totalLength / 10;
+            } else if (Math.abs(previousX - primarySpan) <= totalLength / 10 && previousX - primarySpan < 0) {
+                x = primarySpan;
+            } else if (previousX < primarySpan && previousX !== R1 / load && previousXtwice !== R1 / load && Math.abs(previousX - R1 / load) <= totalLength / 10) {
+                x = R1 / load;
+            } else if (Math.abs(previousX - totalLength) < totalLength / 10) {
+                x = totalLength;
+            } else if (previousX > primarySpan && previousX !== totalLength - (R3 / load) && previousXtwice !== totalLength - (R3 / load) && Math.abs(previousX - (totalLength - (R3 / load))) < totalLength / 10) {
+                x = totalLength - (R3 / load);
+            } else if (previousX === R1 / load || previousX === totalLength - (R3 / load)) {
+                x = previousXtwice + totalLength / 10;
+            } else {
+                x = previousX + totalLength / 10;
+            }
+
+            results.push(x);
+
+            const currentKey = x
+            const previousKey = results[i - 1] || 0
+
+            if (i > 0) {
+                resultsV[i - 1] = countV({ currentKey: previousKey })
+            }
+
+            isCounting = x < totalLength
+
+            if (!isCounting) {
+                resultsV[i] = countV({ currentKey })
+            }
+
+            i++
         }
+
+        function countV({ currentKey }) {
+
+            if (currentKey === 0 || currentKey === totalLength) {
+                return 0;
+            } else {
+                if (currentKey < primarySpan) {
+                    return Math.round(-((R1 * currentKey - 0.5 * load * Math.pow(currentKey, 2))) * 100) / 100;
+                } else if (currentKey > primarySpan) {
+                    return Math.round(-((R1 * currentKey + R2 * (currentKey - primarySpan)) - (0.5 * load * Math.pow(currentKey, 2))) * 100) / 100;
+                } else {
+                    return Math.round(-((R1 * primarySpan - 0.5 * load * Math.pow(primarySpan, 2))) * 100) / 100;
+                }
+            }
+        }
+
+        return {
+            x: results,
+            y: resultsV
+        };
     },
 
     getShearForceEquation: function (beam, load) {
         const { primarySpan, secondarySpan } = beam;
-        const { EI, j2 } = beam.material.properties;
 
         const totalLength = primarySpan + secondarySpan;
         const M1 = -((load * Math.pow(secondarySpan, 3)) + (load * Math.pow(primarySpan, 3))) / (8 * (primarySpan + secondarySpan));
@@ -260,25 +391,19 @@ BeamAnalysis.analyzer.twoSpanUnequal.prototype = {
         function countV({ currentKey, previousKey, nextKey }) {
 
             if (currentKey === 0) {
-                return rounded(R1);
+                return Math.round(R1);
             } else if (currentKey === totalLength) {
-                return rounded((R1 + R2) - (load * totalLength));
+                return Math.round((R1 + R2) - (load * totalLength));
             } else if (currentKey === primarySpan && previousKey - primarySpan < 0) {
-                return rounded(R1 - (load * primarySpan));
+                return Math.round(R1 - (load * primarySpan));
             } else if (currentKey === primarySpan && nextKey - primarySpan > 0) {
-                return rounded((R1 + R2) - (load * primarySpan));
+                return Math.round((R1 + R2) - (load * primarySpan));
             } else if (currentKey < primarySpan) {
-                return rounded(R1 - (load * currentKey));
+                return Math.round(R1 - (load * currentKey));
             } else {
-                return rounded((R1 + R2) - (load * currentKey));
+                return Math.round((R1 + R2) - (load * currentKey));
             }
         }
-
-        function rounded(x) {
-            return Number(x).toFixed(2)
-        }
-
-        console.log({ results, resultsV });
 
         return {
             x: results,
